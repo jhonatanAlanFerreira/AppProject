9a3cd6a1599664e2cd66d5739b01144af33f1831
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-12 10:15:24 -0300

    Edited

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,7 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
   recipes: Recipe[] = [
     new Recipe('A test recipe','this is a simply test','https://previews.123rf.com/images/szefei/szefei1307/szefei130700138/20891507-indian-vegetarian-food-biryani-rice-curry-dhal-and-milk-tea-on-dining-table-.jpg')
+    ,new Recipe('Another teste recipe','this is another simply test','https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG')
   ];
 
   ngOnInit() {
