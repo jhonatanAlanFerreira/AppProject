e7ceda2c6f92346d61113c2e25b97b0f6ddcc038
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-14 21:25:06 -0300

    Added dropdown directive

--- prj-start/src/app/shared/dropdown.directive.ts
+++ prj-start/src/app/shared/dropdown.directive.ts
@@ -1,11 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective {
-  
+export class DropdownDirective implements OnInit {
+  open = false;
 
-  constructor() { }
+  constructor(private renderer:Renderer2, private element:ElementRef) { }
 
+  ngOnInit(){
+    this.renderer.addClass(this.element.nativeElement,"btn-group");
+  }
+
+  @HostListener('click') clicked(){
+    this.open = !this.open;
+    let element = this.element.nativeElement;
+    this.open? this.renderer.addClass(element,"open"): this.renderer.removeClass(element,"open");
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if(!this.element.nativeElement.contains(event.target)){
+      this.open = false;
+      this.renderer.removeClass(this.element.nativeElement,"open");
+    } ;
+  }
 }
