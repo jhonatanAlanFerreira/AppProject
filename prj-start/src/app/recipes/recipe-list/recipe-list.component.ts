28d306d477c760edf9885faad6e3c3bd9378ccfb
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-21 22:44:04 -0300

    Adicionado lista de ingredientes

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,8 +11,22 @@ export class RecipeListComponent implements OnInit {
 
   constructor() { }
   recipes: Recipe[] = [
-    new Recipe('A test recipe','this is a simply test','https://previews.123rf.com/images/szefei/szefei1307/szefei130700138/20891507-indian-vegetarian-food-biryani-rice-curry-dhal-and-milk-tea-on-dining-table-.jpg')
-    ,new Recipe('Another teste recipe','this is another simply test','https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG')
+   
+    new Recipe('A test recipe',
+    'this is a simply test',
+    'https://previews.123rf.com/images/szefei/szefei1307/szefei130700138/20891507-indian-vegetarian-food-biryani-rice-curry-dhal-and-milk-tea-on-dining-table-.jpg',
+    [
+    new Ingredient("Tomates",10),
+    new Ingredient("Arroz",3)
+    ]),
+   
+    new Recipe('Another teste recipe',
+    'this is another simply test',
+    'https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG',
+    [
+      new Ingredient("Alface",15),
+      new Ingredient("Peixe",7)
+      ])
   ];
 
   ngOnInit() {
