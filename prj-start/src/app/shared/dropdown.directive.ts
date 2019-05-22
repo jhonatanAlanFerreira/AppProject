61b71ecb583f7a72b823c3c8925f5e63c4035986
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-21 21:24:49 -0300

    Dropdown Original

--- prj-start/src/app/shared/dropdown.directive.ts
+++ prj-start/src/app/shared/dropdown.directive.ts
@@ -1,34 +1,15 @@
-import { Directive, ElementRef, Renderer2, HostListener, OnInit, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 
-export class DropdownDirective implements OnInit {
-  open = false;
-  @Input('appDropdown') name:string;
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
 
-  constructor(private renderer:Renderer2, private element:ElementRef) { }
-
-  ngOnInit(){
-
-    this.renderer.setProperty(this.element.nativeElement,'innerHTML',
-    '<button class="btn btn-primary dropdown-toggle">'+this.name+' <span class="caret"></span></button><ul class="dropdown-menu">'
-    +this.element.nativeElement.innerHTML+'</ul>'
-    );
-
-    this.renderer.addClass(this.element.nativeElement,"btn-group");
-  }
-
-  @HostListener('document:click', ['$event'])
-  click(event) {
-    if(!this.element.nativeElement.contains(event.target)){
-      this.open = false;
-      this.renderer.removeClass(this.element.nativeElement,"open");
-    }else{
-      this.open = !this.open;
-      let element = this.element.nativeElement;
-      this.open? this.renderer.addClass(element,"open"): this.renderer.removeClass(element,"open");
-    } ;
+  constructor(private element:ElementRef) {}
+  
+  @HostListener('document:click', ['$event']) clicked (event){
+   this.element.nativeElement.contains(event.target)? this.isOpen = !this.isOpen : this.isOpen = false;
   }
 }
